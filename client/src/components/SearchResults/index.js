102a1a0765d09a537529f22660462174309bb3b6
Charles Tuggle <tuggle.charles@gmail.com>
2019-12-02 13:48:35 -0500

    updated search results components for displaying products

--- client/src/components/SearchResults/index.js
+++ client/src/components/SearchResults/index.js
@@ -7,6 +7,12 @@ function SearchResults(props) {
       {props.results.map(result => (
         <li key={result.id} className="list-group-item">
           <img alt="Dog" src={result.url} className="img-fluid" />
+          <div className="productText">
+            <p>Product Name: {result.product_name}</p>
+            <p>Product Description: {result.description}</p>
+            <p>Price: ${result.price}</p>
+            <button className="addButton">Add to Shopping Cart</button>
+          </div>
         </li>
       ))}
     </ul>
